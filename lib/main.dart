508560508c81b7baeccdb5308782a37173639c48
haqqer <ilhamserbaguna@gmail.com>
2019-12-19 01:32:50 +0700

    update for edit cashform

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:cashflow/pages/cashform.dart';
+import 'package:cashflow/pages/riwayat.dart';
 import 'package:flutter/material.dart';
 import 'package:cashflow/pages/home.dart';
 
 void main() => runApp(MyApp());
+
+final key = GlobalKey<CashFormState>();
+final riwayatKey = GlobalKey<RiwayatState>();
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
